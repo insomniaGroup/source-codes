e334ee71e0a6006b79a1eb6e2a5fe4dded91d1d0
Akhand Pratap Singh <144223699+coderakhand@users.noreply.github.com>
2024-10-06 23:31:49 +0530

    Update ceil_floor_div.cpp

--- ceil_floor_div.cpp
+++ ceil_floor_div.cpp
@@ -1,3 +1,4 @@
+//Seem Doubtful and Correct at the same time
 
 ll ceil_div(ll a, ll b) {
   return a / b + ((a ^ b > 0) && a % b != 0);
@@ -5,4 +6,13 @@ ll ceil_div(ll a, ll b) {
 
 ll floor_div(ll a, ll b) {
   return a / b - ((a ^ b < 0) && a % b != 0);
+}
+
+
+ll floor_div(ll a, ll b) {
+    return a / b - (a % b != 0 && ((a < 0) != (b < 0)));
+}
+
+ll ceil_div(ll a, ll b) {
+    return a / b + (a % b != 0 && ((a < 0) == (b < 0)));
 }
