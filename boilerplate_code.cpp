6b21108f80949f38a8ef4afabbc9fb8b394d1dda
Akhand Pratap Singh <144223699+coderakhand@users.noreply.github.com>
2024-10-07 23:31:44 +0530

    Create boilerplate_code.cpp

---
+++ boilerplate_code.cpp
@@ -0,0 +1,56 @@
+#pragma GCC optimize("O3")
+#ifndef ONLINE_JUDGE
+#pragma GCC target("arch=armv8-a")
+#endif
+#ifdef __x86_64__
+#pragma GCC target("sse4")
+#endif
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef complex<ld> cd;
+typedef pair<int, int> pi;
+typedef pair<ll,ll> pl;
+typedef pair<ld,ld> pd;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ld> vd;
+typedef vector<ll> vl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+typedef vector<cd> vcd;
+
+template<class T> using pq = priority_queue<T>;
+template<class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+
+#define forloop(i, a, b) for (int i = a; i < (b); i++)
+#define put(x)  cout << x << '\n'
+#define yes cout << "YES" << '\n'
+#define no cout << "NO" << '\n'
+#define all(x) x.begin(), x.end()
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+void solve()
+{
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+#ifndef coderakhand_DEBUG
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+#endif
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
