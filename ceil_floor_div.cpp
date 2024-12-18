34a21e1c1fbb08c431d37547b9c4a261516f1f08
Abhay Pratap Singh <138102159+drrakonov@users.noreply.github.com>
2024-12-18 06:48:08 +0530

    Update ceil_floor_div.cpp
    bracket was missing in the return value and because of that missing bracket ceil_div was failing,
    now bracket is included.

--- ceil_floor_div.cpp
+++ ceil_floor_div.cpp
@@ -1,7 +1,7 @@
 //Seem Doubtful and Correct at the same time
 
 ll ceil_div(ll a, ll b) {
-  return a / b + ((a ^ b > 0) && a % b != 0);
+  return a / b + ((a ^ b) > 0 && a % b != 0);
 }
 
 ll floor_div(ll a, ll b) {
